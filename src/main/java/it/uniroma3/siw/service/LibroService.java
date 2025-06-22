c52f4ca749c72163c4674407a7807413fad7a210
fedem <fed.mecucci@stud.uniroma3.it>
2025-06-22 19:25:44 +0200

    aggiunta package iniziali

---
+++ src/main/java/it/uniroma3/siw/service/LibroService.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.repository.LibroRepository;
+
+@Service
+public class LibroService {
+
+	@Autowired
+	private LibroRepository libroRepository;
+}
